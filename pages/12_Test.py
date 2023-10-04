e16b080000e799b9f74c63c82bc2b053d98052e2
tanzilcoding <89991847+tanzilcoding@users.noreply.github.com>
2023-10-04 23:47:22 +0600

    Add files via upload

--- pages/12_Test.py
+++ pages/12_Test.py
@@ -1,8 +1,18 @@
 import streamlit as st
+import os.path
 
 try:
     # import scraper
     file = open('myfile.txt', 'w+')
+    L = ["This is Delhi \n", "This is Paris \n", "This is London"]
+    file.writelines(L)
+    file.close()
+
+    file1 = open("myfile.txt", "r")
+    print("Output of Readlines after appending")
+    st.info(file1.read())
+    file1.close()
+
 # except ImportError:
 #     print("Oops! Cannot find the scraper.py file")
 except Exception as e:
